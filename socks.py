6b2f90bf3fa0297eb8df516e1ad0720529975442
Benjamin Koehler <bkoehler751@gmail.com>
2021-01-11 09:58:09 -0700

    updated socks

--- socks.py
+++ socks.py
@@ -1,5 +1,5 @@
-socks = input("Enter set of numbers seperated by spaces ").strip().split()
+socks = [2,3,3,4,4,4, 'a', 'a']
 pairs = 0
 for element in set(socks):
     pairs += socks.count(element) // 2
-print(f'The amount of pairs you have is {(pairs)}')
+print(f'The amount of pairs you have is {(pairs)}') # should get 3 pairs
